4a39828f54810a8a69d35e38dc57ae298a7bc714
chengpeng <454069019@qq.com>
2019-11-07 17:38:04 +0800

    first commit

---
+++ MegalabClient/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MegalabClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lbtest = new System.Windows.Forms.Label();
+            this.btnDoDeleGate = new System.Windows.Forms.Button();
+            this.tbDataSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnCommSet = new System.Windows.Forms.Button();
+            this.tbCommInfo = new System.Windows.Forms.TextBox();
+            this.btnCommInfoClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnTest);
+            this.groupBox1.Controls.Add(this.lbtest);
+            this.groupBox1.Controls.Add(this.btnDoDeleGate);
+            this.groupBox1.Controls.Add(this.tbDataSend);
+            this.groupBox1.Controls.Add(this.btnSend);
+            this.groupBox1.Controls.Add(this.btnCommSet);
+            this.groupBox1.Location = new System.Drawing.Point(49, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 239);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "通信";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(241, 174);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 5;
+            this.btnTest.Text = "测试";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lbtest
+            // 
+            this.lbtest.AutoSize = true;
+            this.lbtest.Location = new System.Drawing.Point(139, 179);
+            this.lbtest.Name = "lbtest";
+            this.lbtest.Size = new System.Drawing.Size(41, 12);
+            this.lbtest.TabIndex = 4;
+            this.lbtest.Text = "label1";
+            // 
+            // btnDoDeleGate
+            // 
+            this.btnDoDeleGate.Location = new System.Drawing.Point(38, 117);
+            this.btnDoDeleGate.Name = "btnDoDeleGate";
+            this.btnDoDeleGate.Size = new System.Drawing.Size(75, 23);
+            this.btnDoDeleGate.TabIndex = 3;
+            this.btnDoDeleGate.Text = "执行";
+            this.btnDoDeleGate.UseVisualStyleBackColor = true;
+            this.btnDoDeleGate.Click += new System.EventHandler(this.btnDoDeleGate_Click);
+            // 
+            // tbDataSend
+            // 
+            this.tbDataSend.Location = new System.Drawing.Point(263, 44);
+            this.tbDataSend.Name = "tbDataSend";
+            this.tbDataSend.Size = new System.Drawing.Size(100, 21);
+            this.tbDataSend.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(141, 44);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnCommSet
+            // 
+            this.btnCommSet.Location = new System.Drawing.Point(38, 44);
+            this.btnCommSet.Name = "btnCommSet";
+            this.btnCommSet.Size = new System.Drawing.Size(75, 23);
+            this.btnCommSet.TabIndex = 0;
+            this.btnCommSet.Text = "建立";
+            this.btnCommSet.UseVisualStyleBackColor = true;
+            this.btnCommSet.Click += new System.EventHandler(this.btnCommSet_Click);
+            // 
+            // tbCommInfo
+            // 
+            this.tbCommInfo.Location = new System.Drawing.Point(574, 31);
+            this.tbCommInfo.MinimumSize = new System.Drawing.Size(200, 300);
+            this.tbCommInfo.Multiline = true;
+            this.tbCommInfo.Name = "tbCommInfo";
+            this.tbCommInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbCommInfo.Size = new System.Drawing.Size(200, 333);
+            this.tbCommInfo.TabIndex = 1;
+            // 
+            // btnCommInfoClear
+            // 
+            this.btnCommInfoClear.Location = new System.Drawing.Point(699, 390);
+            this.btnCommInfoClear.Name = "btnCommInfoClear";
+            this.btnCommInfoClear.Size = new System.Drawing.Size(75, 23);
+            this.btnCommInfoClear.TabIndex = 2;
+            this.btnCommInfoClear.Text = "清空";
+            this.btnCommInfoClear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCommInfoClear);
+            this.Controls.Add(this.tbCommInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCommSet;
+        private System.Windows.Forms.TextBox tbCommInfo;
+        private System.Windows.Forms.Button btnCommInfoClear;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbDataSend;
+        private System.Windows.Forms.Button btnDoDeleGate;
+        private System.Windows.Forms.Label lbtest;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
---
+++ MegalabClient/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+using System.Configuration;
+
+namespace MegalabClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          // Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        Socket socketSend;
+        string host = "192.168.1.139";
+
+        private void btnCommSet_Click(object sender, EventArgs e)
+        {
+            IPAddress IPWasher = IPAddress.Parse(host);
+            var message = "";
+            try
+            {
+                //创建客户端Socket，获得远程ip和端口号
+                socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socketSend.Connect(IPWasher, 8090);
+
+                var buffer = new byte[1024];//设置一个缓冲区，用来保存数据
+
+                socketSend.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback((ar) =>
+                {
+                    var length = socketSend.EndReceive(ar);
+                    message = Encoding.Unicode.GetString(buffer, 0, length);
+                    OutputLine(message);
+                }), null);
+                OutputLine("Connnet to server");
+                socketSend.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveMessage), socketSend);
+            }
+            catch (Exception)
+            {
+                OutputLine("IP或者端口号错误...");
+            }
+        }
+
+
+        static byte[] buffer = new byte[1024];
+        public  void ReceiveMessage(IAsyncResult ar)
+        {
+            try
+            {
+                var socket = ar.AsyncState as Socket;
+                var length = socket.EndReceive(ar);
+                var message = Encoding.Unicode.GetString(buffer, 0, length);
+                OutputLine(message);
+                //嵌套调动
+                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveMessage), socket);
+            }
+            catch (Exception ex)
+            {
+                OutputLine(ex.Message);
+            }
+        }
+
+        public  void OutputLine(String str)
+        {
+            Action<string> ac = (strLine) =>
+            {
+                  strLine = (strLine.Replace("\r\n", "\n").Replace("\n", "\r\n")) + "\r\n";
+
+                  int charsToRemove = (tbCommInfo.TextLength + strLine.Length) - tbCommInfo.MaxLength;
+
+                  if (charsToRemove > 0)
+                  {
+                      tbCommInfo.Text = tbCommInfo.Text.Remove(0, tbCommInfo.Text.IndexOf("\r\n", charsToRemove) + 2);
+                  }
+
+                  tbCommInfo.AppendText(strLine);
+                  tbCommInfo.AppendText("\r\n");
+              };
+            Invoke(ac, str);
+        }
+
+        private void OutputLine(String strErrorText, Exception exception)
+        {
+            OutputLine(strErrorText);
+            OutputLine("/==============================\\");
+            OutputLine("Error Code: " + exception.Message);
+            OutputLine("------------------------------");
+            OutputLine("Error Text:");
+            OutputLine("\t" + exception.Message.Replace("\r\n", "\n").Replace("\n", "\r\n\t"));
+            OutputLine("\\==============================/");
+        }
+        private void btnCommInfoClear_Click(object sender, EventArgs e)
+        {
+            tbCommInfo.Clear();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string msg = tbDataSend.Text.Trim();
+                socketSend.Send(Encoding.Unicode.GetBytes(msg));
+            }
+            catch { }
+        }
+
+
+        delegate void feedbcak(int value);
+
+        void feedbacktooutput(int ii)
+        {
+            OutputLine("res is "+ ii.ToString());
+        }
+
+
+
+        void counter(feedbcak fb)
+        {
+            for(int ii = 0;ii<5;ii++ )
+            {
+                if(fb!=null)
+                {
+                    fb(33);
+                }
+            }
+        }
+        private void btnDoDeleGate_Click(object sender, EventArgs e)
+        {
+            //counter(new feedbcak (feedbacktooutput));
+            // feedbcak myfb = new feedbcak(feedbacktooutput);
+            //counter((int ii) => OutputLine("what a fuck"));
+
+            // test mytest_1 = new test(mytest);
+            //mytest_1.Invoke();
+            //hertest(new test(mytest));
+            //hertest((string str)=>OutputLine("Hello world"));
+            //actiontest();
+            //threadaction();
+            //OutputLine("hello");
+
+            doit();
+        }
+
+
+        void threadaction()
+        {
+            Thread th = new Thread((object o) =>
+            {
+                int mm = Convert.ToInt32(o);
+                   for (int ii = 0; ii < mm; ii++)
+                   {
+                       Action ac = () =>
+                       {
+                           OutputLine("thread ii is " + ii.ToString());
+                       };
+                       Invoke(ac);
+                       Thread.Sleep(1000);
+                   }
+            }
+            );
+            tbDataSend.Text = "HHH";
+            th.Start(8);
+        }
+
+        delegate void test(string str);
+        void mytest(string str)
+        {
+            OutputLine("my name is .."+ str);
+        }
+
+        void hertest(test tt)
+        {
+            if(tt!=null)
+            {
+                tt("HH");
+            }
+        }
+
+        void actiontest()
+        {
+            Action<string> myaction = new Action<string>(mytest);
+            myaction("hh");
+        }
+
+        delegate void deletest(int ii);
+
+        void add_1(int ii)
+        {
+            OutputLine("add_1 is "+ ii.ToString());
+        }
+
+        void add_2(int ii)
+        {
+            OutputLine("add_2 is " + ii.ToString());
+        }
+
+
+        void doit()
+        {
+            deletest mydel = new deletest(add_1);
+            mydel += add_2;
+            mydel(44);
+        }
+
+
+        public delegate int AddHandler(int a, int b);
+
+
+       public   int adder(int a,int b)
+        {
+            Action ac = () =>
+            {
+               OutputLine("res is " + (a + b).ToString());
+            };
+            //ac.Invoke();
+            Invoke(ac);
+            
+            return a + b;
+        }
+
+        void test_1()
+        {         
+           AddHandler handler = new AddHandler(adder);
+            //异步操作接口(注意BeginInvoke方法的不同！)
+            IAsyncResult result = handler.BeginInvoke(1, 2, new AsyncCallback(cbfunc), "AsycState:OK");
+            Console.WriteLine("继续做别的事情。。。");
+            //Thread.Sleep(10000);
+            OutputLine("end");
+            //Console.ReadKey();
+        }
+        public  void cbfunc(IAsyncResult result)
+        {
+            //result 是“加法类.Add()方法”的返回值
+            //AsyncResult 是IAsyncResult接口的一个实现类，空间：System.Runtime.Remoting.Messaging
+            //AsyncDelegate 属性可以强制转换为用户定义的委托的实际类。
+            AddHandler handler = (AddHandler)((AsyncResult)result).AsyncDelegate;
+            //Console.WriteLine(handler.EndInvoke(result));
+            //Console.WriteLine(result.AsyncState);
+            Action ac = () =>
+            {
+                OutputLine("hh is "+ handler.EndInvoke(result).ToString());
+                OutputLine((string)result.AsyncState);
+            };
+            Invoke(ac);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            //test_1();
+            //mytest();
+            appconfigtest();
+        }
+
+
+        void appconfigtest()
+        {
+            //for(int ii = 0;ii<ConfigurationManager.ConnectionStrings.Count;ii++)
+            //{
+            //    string name = ConfigurationManager.ConnectionStrings[ii].Name;
+            //    string str = ConfigurationManager.ConnectionStrings[ii].ConnectionString;
+            //     OutputLine(name+" "+ str);
+            //    OutputLine("over"); ;
+            //}
+
+
+            string[] keys = ConfigurationManager.AppSettings.AllKeys;
+            foreach(string Key in keys)
+            {
+                OutputLine("key:"+Key + ", Value:"+ConfigurationManager.AppSettings[Key]);
+            }
+
+        }
+
+
+        public delegate string delegateexample(int a,int b);
+
+
+        public string myadd(int a,int b)
+        {
+
+            for(int ii = 0;ii< 6; ii ++)
+            {
+                OutputLine("Cycle "+ ii.ToString());
+                Thread.Sleep(1000);
+            }
+            return (a + b).ToString();
+        }
+
+        public void mytest()
+        {
+            delegateexample mydeletest = myadd;
+            IAsyncResult res =  mydeletest.BeginInvoke(1,4, new AsyncCallback(cb), mydeletest);
+
+            //while(res.IsCompleted == false)
+            //{
+            //    OutputLine("wait of end");
+            //    Thread.Sleep(500);
+            //}
+
+            //string ss = mydeletest.EndInvoke(res);
+            //OutputLine("end invoke is "+ss);
+        }
+
+        public void cb(IAsyncResult ia)
+        {
+
+            delegateexample tmp = ia.AsyncState as delegateexample;
+            string res = tmp.EndInvoke(ia);
+            OutputLine("res is cb" + res);
+        }
+
+
+    }
+}
